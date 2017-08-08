7b4ce9ba244e448c0ee342d19d46f3cfeeb84654
hustchenshu <569874886@qq.com>
2017-08-08 21:47:24 +0800

    version 1.1

--- mypro/src/components/blog/Blog.vue
+++ mypro/src/components/blog/Blog.vue
@@ -10,7 +10,23 @@
         <div v-show="show">
           <div class="md markdown-body" v-html='content'>
           </div>
-          <div class="footer"><a href="javascript:void()">shouqi</a></div>
+          <div class="footer">
+            <div class="collapse">          
+              <a href="javascript:void(0)" @click="change">
+              <svg t="1502196099740" class="icon" style="width: 2em; height: 1.5em;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" ><path d="M943.118018 489.30828c0 24.569524-19.923848 44.540694-44.496392 44.540694-14.193778 0-26.855108-6.649338-34.988565-17.02609L520.821649 173.560397l0 657.349042c0 1.578765-0.084577 3.114234-0.250709 4.648696-2.487964 22.377578-22.618219 39.890991-47.110214 39.890991-24.488975 0-44.61923-17.51442-47.110214-39.890991-0.043295-0.332266-0.081556-0.663524-0.081556-0.99579L426.268955 205.770014 114.338098 517.450161c-0.041281 0.043295-0.081556 0.125858-0.125858 0.169153-8.134464 9.913594-20.503802 16.22966-34.327054 16.22966-24.610805 0-44.534653-19.971171-44.534653-44.540694 0-12.332085 4.981969-23.498219 13.071124-31.545086l-0.206407-0.213455L489.73066 16.45117l0 0.038261 0.084577-0.038261 53.752504 53.796806 9.170528 9.167507 377.595582 378.134255-0.250709 0.250709C938.135042 465.853356 943.118018 477.01949 943.118018 489.30828zM426.350511 848.010037l-0.081556 0 0-0.99579C426.268955 847.346513 426.306209 847.678779 426.350511 848.010037zM520.821649 843.361341l0 4.648696-0.250709 0C520.737073 846.475575 520.821649 844.940105 520.821649 843.361341zM974.166719 972.992211c0 26.024444-19.926869 47.15351-44.537674 47.15351L52.783396 1020.14572c-1.785172 0-3.56833-0.124851-5.313227-0.331259-22.082567-2.782975-39.224446-22.709844-39.224446-46.822251 0-24.119455 17.140873-44.002022 39.224446-46.777949 1.744897-0.207414 3.528056-0.332266 5.313227-0.332266l876.845649 0C954.23985 925.881996 974.166719 946.967767 974.166719 972.992211zM426.268955 839.714475l0 7.299772c-0.127872-1.203204-0.166133-2.406408-0.166133-3.652907C426.102822 842.119876 426.140076 840.916672 426.268955 839.714475z"></path></svg>收起
+              </a>
+            </div>
+            <div class="bshare-custom">
+              <a title="分享到新浪微博" class="bshare-sinaminiblog"></a>
+              <a title="分享到豆瓣" class="bshare-douban"></a>
+              <a title="分享到有道笔记" class="bshare-youdaonote"></a>
+              <a title="分享到搜狐微博" class="bshare-sohuminiblog"></a>
+              <a title="分享到网易微博" class="bshare-neteasemb"></a>
+              <a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a>
+              <span class="BSHARE_COUNT bshare-share-count">0</span>
+            </div>
+            <a class="bshareDiv" onclick="javascript:return false;"></a>           
+          </div>
         </div>
       </transition>
     </div>
@@ -124,6 +140,7 @@ export default {
     }
   }
   .content{
+    padding-bottom: 20px;
     background: rgba(255, 255, 244, 0.5);
     border-radius: 0 10px 10px 10px;
     // box-shadow: 2px -2px gray;
@@ -150,13 +167,27 @@ export default {
       // padding-bottom: 45px;
     }
     .footer{
+      width: 100%;
       position: absolute;
       border-radius: 0 0 10px 10px;
       background: rgba(255, 255, 255, 0.5);
       bottom: 0px;
       height: 45px;
-      border-top: solid 1px;
-      box-shadow: 2px 2px gray;
+      .bshare-custom{
+        margin:5px 0 0 10px; 
+      }
+      .collapse{
+        color:#707070;
+        a{
+          float: right;
+          margin: 5px 10px 0 0;
+          text-decoration: none;
+          color:inherit;
+        }
+      }
+      .collapse:hover{
+        color:#809090;
+      }
     }
   }
 }
--- mypro/src/components/frame/frame.vue
+++ mypro/src/components/frame/frame.vue
@@ -16,22 +16,29 @@
             <router-view></router-view>
             </transition>
           </div>
+
+          <div class="footer">
+            <div class="copyright">
+              © 2017 HustChenShu, Inc. <br>Based on Vue and Express.<br>
+              <a href="http://www.miitbeian.gov.cn" target='_blank'>鄂ICP备17017286号-1</a>
+            </div>
+            <div class="links">
+              <div class="github">
+              <a href="https://github.com/hustchenshu" target="_blank">
+                <svg aria-hidden="true" class="mark" height="24" version="1.1" viewBox="0 0 16 16" width="24"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+              </a>
+            </div>
+            <div class="csdn-svg">
+              <a href="http://blog.csdn.net/dujiajiyiyi" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" class="mark" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" width="32.41" height="28.313" viewBox="0 0 32.41 28.313"><path d="M32.392,1.067 C32.118,3.080 31.876,4.863 31.571,7.111 C27.992,6.559 24.431,6.049 20.851,6.345 C17.574,6.616 14.398,7.235 12.079,9.747 C7.821,14.358 10.138,20.714 16.570,21.848 C20.574,22.555 24.627,22.174 28.643,21.624 C28.488,24.734 28.480,24.260 28.340,27.301 C28.310,27.966 28.462,27.705 23.753,28.183 C18.758,28.433 13.767,28.493 8.980,26.720 C4.444,25.041 0.872,22.370 0.148,17.436 C-0.597,12.358 1.533,8.258 5.612,5.096 C11.733,0.349 22.443,-1.321 32.392,1.067 Z" class="cls-1"/></svg>
+              </a>
+            </div>
+            </div>
+          </div>
+
+
         </div>
-        <div class="footer">
-          <div class="copyright">
-            © 2017 GitHub, Inc. 
-          </div>
-          <div class="github">
-            <a href="https://github.com/hustchenshu" target="_blank">
-              <svg aria-hidden="true" class="mark" height="24" version="1.1" viewBox="0 0 16 16" width="24"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>hustchenshu
-            </a>
-          </div>
-          <div class="csdn-svg">
-            <a href="http://blog.csdn.net/dujiajiyiyi" target="_blank">
-              <svg xmlns="http://www.w3.org/2000/svg" class="mark" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid" width="32.41" height="28.313" viewBox="0 0 32.41 28.313"><path d="M32.392,1.067 C32.118,3.080 31.876,4.863 31.571,7.111 C27.992,6.559 24.431,6.049 20.851,6.345 C17.574,6.616 14.398,7.235 12.079,9.747 C7.821,14.358 10.138,20.714 16.570,21.848 C20.574,22.555 24.627,22.174 28.643,21.624 C28.488,24.734 28.480,24.260 28.340,27.301 C28.310,27.966 28.462,27.705 23.753,28.183 C18.758,28.433 13.767,28.493 8.980,26.720 C4.444,25.041 0.872,22.370 0.148,17.436 C-0.597,12.358 1.533,8.258 5.612,5.096 C11.733,0.349 22.443,-1.321 32.392,1.067 Z" class="cls-1"/></svg>hustchenshu
-            </a>
-          </div>
-        </div>
+
       </div>
     </div>
     <div class="toTop">
@@ -75,7 +82,82 @@ export default {
 </script>
 
 <style lang="less" scoped>
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td
+{
+    font-family: inherit;
+    font-size: 100%;
+    font-weight: inherit;
+    font-style: inherit;
 
+    margin: 0;
+    padding: 0;
+
+    vertical-align: baseline;
+
+    border: 0;
+    outline: 0;
+}
+body
+{
+    font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+
+}
 
 .slide-fade-enter-active{
   transition: all .5s ease;
@@ -155,39 +237,57 @@ export default {
 }
 
 .footer{
-  position: relative;
+  position: absolute;
   height: 120px;
-  width: 100%;
+  width: 100vw;
+
+  margin: 0 auto;
+
+  // bottom: 0;
+  left: 50%;
+  transform:translate(-50%);
   background: #2d2d2d;
-  .copyright{
-    position: absolute;
-    right: 10px;
-    bottom: 1em;
-  }
-  .github{
-    padding-top: 40px;
-  }
-  .github,.csdn-svg{
-    font-size: 16px;
-    margin: auto;
-    // padding-top: 20px;
-    width: 160px;
-    height: 24px;
-    a{
-      text-decoration: none;
-      color: gray;
-      text-align: center;
-      vertical-align: middle;
-      .mark{
-        fill: #c6cbd1;
-        width: 16px;
-        height: 16px;
-      }
-      .mark:hover{
-        fill: #cc9982;
+  text-align: center;
+  .links{
+    // float: left;
+    .github{
+      padding: 10px 20px 20px 5px;
+    }
+    .github,.csdn-svg{
+      font-size: 6px;
+      margin: auto;
+      display: inline-block;
+      // padding-top: 20px;
+      // width: 160px;
+      // height: 24px;
+      a{
+        text-decoration: none;
+        color: gray;
+        text-align: center;
+        vertical-align: middle;
+        .mark{
+          fill: #c6cbd1;
+          width: 32px;
+          height: 32px;
+        }
+        .mark:hover{
+          fill: #cc9982;
+        }
       }
     }
   }
+  .copyright{
+    position: absolute;
+    // right: 10px;
+    a{
+      text-decoration: none;
+      color:black;
+    }
+    left:50%;
+    transform:translate(-50%); 
+    bottom: 1em;
+  }
+
 }
 
 .toTop{
@@ -205,82 +305,8 @@ export default {
   }
 }
 
+#app,.container{
+  min-height: 100vh;
 
-
-
-html,
-body,
-div,
-span,
-applet,
-object,
-iframe,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-blockquote,
-pre,
-a,
-abbr,
-acronym,
-address,
-big,
-cite,
-code,
-del,
-dfn,
-em,
-img,
-ins,
-kbd,
-q,
-s,
-samp,
-small,
-strike,
-strong,
-sub,
-sup,
-tt,
-var,
-dl,
-dt,
-dd,
-ol,
-ul,
-li,
-fieldset,
-form,
-label,
-legend,
-table,
-caption,
-tbody,
-tfoot,
-thead,
-tr,
-th,
-td
-{
-    font-family: inherit;
-    font-size: 100%;
-    font-weight: inherit;
-    font-style: inherit;
-
-    margin: 0;
-    padding: 0;
-
-    vertical-align: baseline;
-
-    border: 0;
-    outline: 0;
-}
-body
-{
-    font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
 }
 </style>
